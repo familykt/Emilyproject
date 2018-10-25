68a3086567c64ef8628b2aedadd0680e3016f3ef
John Tripla <funfunfunfun@naver.com>
2018-10-25 19:17:20 +0900

    make it better

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var app = express()
-var port = process.env.PORT
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-app.get('/',function(req,res){
-  res.send('hello,world')
+const app = express();
+const port = process.env.PORT
+const button = {
+  type : 'buttons',
+  buttons :[
+    '버튼1',
+    '버튼2',
+    '정보'
+]
+}
+app.use(bodyParser.urlencoded({extented:false}));
+app.use(bodyParser.json());
+
+app.get('/keyboard',function(req,res){
+  res.json(buttons);
 });
 
-app.listen(port);
+app.post('/messasge',function(req,res){
+  console.log(req.body);
+res.json(null)
+})
+
+
+http.createServer(app).listen(port,function(
+  console.log('server start')
+));
